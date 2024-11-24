2e8f21c1f8dc2569599ba0078f2da055b931edbe
Samy Mokhtari <samy.mokhtari213@gmail.com>
2024-11-24 20:03:15 +0100

    [resolve #9] Create beautiful backup and send it beautifully to Google Drive

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from utils.fetch_config import fetch_config
 from business_logic.create_backup import create_backup, regroup_backups
+from business_logic.gdrive_service import GoogleDriveService
 from utils.logger import setup_logger
-from logging import Logger, INFO
+from logging import Logger, INFO, DEBUG
 from typing import List
 from datetime import date
 import os
+
+
 def main():
     # Setup logger
-    logger: Logger = setup_logger(name="backup2gdrive", log_file="logs/backup2gdrive.log", level=INFO)
+    logger: Logger = setup_logger(
+        name="backup2gdrive",
+        log_file="logs/backup2gdrive.log",
+        level=DEBUG if str(os.environ.get("ENV")).upper() == "DEV" else INFO,
+    )
+
     logger.info("Starting Backup2GDrive script. \nFetch config...")
 
     # Fetch config
     try:
         config = fetch_config()
-    except FileNotFoundError as e: 
-        logger.error("File not found: %s", e)   
-        exit(1) 
+    except FileNotFoundError as e:
+        logger.error("File not found: %s", e)
+        exit(1)
     except ValueError as e:
         logger.error("Invalid config file: %s", e)
         exit(2)
@@ -31,8 +39,29 @@ def main():
 
     # Regroup backups
     backups_dir = os.path.join(os.getcwd(), "backups")
+    backups_filepath = os.path.join(
+        backups_dir,
+        f"BACKUP_{config.project_name.upper()}_{
+                                    date.today().strftime('%Y%m%d')}.zip",
+    )
     if not os.path.exists(backups_dir):
         os.makedirs(backups_dir)
-    regroup_backups(archived_filepaths, os.path.join(os.getcwd(), "backups", f"BACKUP_{config.project_name.upper()}_{date.today().strftime('%Y%m%d')}.zip"))
+    regroup_backups(archived_filepaths, backups_filepath)
+
+    google_drive_service = GoogleDriveService(config.users_emails)
+    # Remove old backups
+    logger.info("Removing old backups...")
+    removed_backups_count = google_drive_service.remove_old_files(
+        days_old=config.days_to_keep
+    )
+    logger.info(f"Removed {removed_backups_count} old backups.")
+
+    # Upload new backups
+    logger.info("Uploading backups to Google Drive...")
+    google_drive_service.upload_file(backups_filepath, config.g_drive_destination_path)
+
+    logger.info("Backup process completed successfully.")
+
+
 if __name__ == "__main__":
     main()
